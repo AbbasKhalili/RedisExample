241f9fdff6fbd0ce36ae36017d895a0369f5c348
AbbasKhalili <khalilikhoo2@gmail.com>
2020-10-01 12:42:13 +0330

    init

---
+++ caching/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace caching.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IDistributedCache _cache;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            WeatherForecast[] weatherResult;
+
+            var cached = await _cache.GetStringAsync("weatherResult");
+            if (cached == null)
+            {
+                var rng = new Random();
+                weatherResult = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                    {
+                        Date = DateTime.Now.AddDays(index),
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    })
+                    .ToArray();
+                var result = JsonSerializer.Serialize(weatherResult);
+                await _cache.SetStringAsync("weatherResult", result);
+            }
+            else
+            {
+                weatherResult = JsonSerializer.Deserialize<WeatherForecast[]>(cached);
+            }
+
+            return weatherResult;
+        }
+    }
+}
